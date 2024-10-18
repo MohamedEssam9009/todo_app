e0a536a09c111d3bf1fba883ef7d86224ae6eba6
Mohamed Essam <77198018+MohamedEssam9009@users.noreply.github.com>
2024-10-19 00:11:09 +0300

    - added bloc observer

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
+import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:todo_app/layout/home_layout.dart';
 
+import 'shared/bloc_observer.dart';
+
 void main() {
+  Bloc.observer = MyBlocObserver();
+
   runApp(const MyApp());
 }
 
